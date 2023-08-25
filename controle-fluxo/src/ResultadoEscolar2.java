685cc2f0b4a86ea09b391031e53f77dcd9ee8a99
WilsonFirmino <supperwillf@gmail.com>
2023-08-25 19:35:41 -0300

    Condição Ternaria

---
+++ controle-fluxo/src/ResultadoEscolar2.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolar2 {
+ 
+    public static void main(String[] args) {
+        
+        int nota = 4;
+
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        String resultado2 = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperacão" : "Reprovado";
+        System.out.println(resultado2);
+
+    }
+
+
+}
