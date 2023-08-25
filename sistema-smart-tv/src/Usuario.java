9ec52a84ea91c2b785de1d5ce06f9054a8f4db17
WilsonFirmino <supperwillf@gmail.com>
2023-08-24 21:34:25 -0300

    Projeto SmartTV

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarVolume();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.diminuirCanal();
+        smartTv.diminuirVolume();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(10);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+    }
+}
