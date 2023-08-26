7639351828c5191f80dd8a677a846adc0058668e
WilsonFirmino <supperwillf@gmail.com>
2023-08-26 11:32:26 -0300

    Fluxo de repetição com For

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        for(int contador = 1; contador <= 20; contador++){
+            System.out.println("Contador: " + contador);
+        }
+        
+    }
+}
