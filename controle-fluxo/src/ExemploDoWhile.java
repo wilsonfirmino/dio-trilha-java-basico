20deebfd52baedc5b8cb8f53ac1ef1c1999353cc
WilsonFirmino <supperwillf@gmail.com>
2023-08-26 12:15:24 -0300

    Repetição com Do While

---
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do{
+            //executando repetidas vezes ate alguem atender
+            System.out.println("Telefone tocando");            
+        }while(tocando());
+
+        System.out.println("Alô !!!");
+        
+    }
+
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu?" + atendeu);
+
+        // negando o ato de continuar tocando
+        return ! atendeu;
+    }
+}
