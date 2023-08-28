10f8150a513c1720c40410bcf36d5f1bb9bdaca7
WilsonFirmino <supperwillf@gmail.com>
2023-08-28 16:22:29 -0300

    Desafios de codigos

---
+++ desafios/src/Deposito.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Deposito {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double valor = scanner.nextDouble();
+
+        if (valor > 0) {
+            //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+            System.out.println("Deposito realizado com sucesso!\r\n" + //
+                    "Saldo atual: R$ 500.50" + valor);
+            
+        } else if (valor == 0) {
+            //TODO: Imprimir a mensagem de valor inválido.
+            System.out.println("Encerrando o programa...");
+        } else {
+             //TODO: Imprimir a mensagem de encerrar o programa.
+             System.out.println("Valor invalido! Digite um valor maior que zero.");
+        }
+    }
+    
+}
